f56890a9979eba43c12c2ff201c9689c254dd99b
pat <pat@desktop.lan>
2018-04-10 01:29:18 -0700

    rewrote using pathlib, modified for command line usage and added moving files to correct folder on NAS

--- rename_tv.py
+++ rename_tv.py
@@ -7,21 +7,22 @@ Created on Tue May 30 18:18:53 2017
 Require python 3.6 for os functions
 """
 import os
-
+import shutil
+from pathlib import Path
+from sys import argv
 # os.getcwd()
-directory = '/home/pat/Videos/'
+directory_video = Path('/home/pat/Videos/')
 
 
-def generate(*episodes):
+def generate(episodes):
     '''input: comma seperarted list of episodes on each disc
     output: dictionary with keys that are disc numbers and
     values of matching strings for episodes on each disc'''
-
     targets = {}
     n = 1
     m = 0
     l = 1
-    k = episodes[m] + 1
+    k = int(episodes[m]) + 1
     replace_string = 'E'
     while n <= len(episodes):
         ep_list = list(range(l, k))
@@ -36,36 +37,52 @@ def generate(*episodes):
         replace_string = 'E'
         l = k
         m += 1
-        k = episodes[m] + l
+        k = int(episodes[m]) + l
         n += 1
     return targets
 
 
 def s_a_d(directory, targets):
     '''finds keys of targets and replaces keys with values in selected text'''
-    with os.scandir(directory) as i:
-        for filename in i:
-            # rename files (ignoring file extensions)
-            # filename and extensionname (extension in [1])
-            filename_split = os.path.splitext(filename)
-            filename_zero = str(filename_split[0])
-            extension = str(filename_split[1])
-            for j in targets.keys():
-                if j in filename_zero:
-                    path1 = os.path.join(filename)
-                    path2 = os.path.join(filename_zero.replace(j, targets.get(j) + extension))
-                    os.rename(path1, path2)
-    return(path1, path2)
+    paths_original = []
+    paths_new = []
+    for filename in directory.iterdir():
+        for j in targets.keys():
+            if j in filename.name:
+                path_original = filename
+                path_new = Path(filename.name.replace(j, targets.get(j)))
+                # path_new = os.path.join(filename_zero.replace(j, targets.get(j) + extension))
+                os.rename(path_original,path_new)
+                paths_original.append(path_original)
+                paths_new.append(path_new)
+    return paths_original,paths_new
 # create folder on server and/or move files in folder to server
 # def
+# translates command line arguments into format for generate
+episode_string = argv[1]
+episode_list = episode_string.split(',') # list containing strings
+# episode_list = episode_list[0]
 # runs generate to create dictionary for what to find and replace
-dict = generate(8, 7, 7)
+episode_list_formatted = generate(episode_list)
 
-# feed directory and dictionary to s_a_d function
-#s_a_d(directory, dict)
-# create folder on server and/or move files in folder to server
-    # try mkdir()
-    # except  FileExistsError:
-        #    break
+paths_discs_original, paths_discs_new = s_a_d(directory_video, episode_list_formatted)
+#operations to get show name
+disc_number = list(episode_list_formatted.keys())
+season_number = argv[2].upper()
+disc_identifier = '_'+season_number+disc_number[-1]
+path_show = paths_discs_original[0].stem
+for d in disc_number:
+    disc_identifier = '_'+season_number+d
+    path_show = path_show.replace(disc_identifier,'')
+# create folder on server if needed
+tv_directory = '/home/pat/NAS/TV/'
+try:
+    os.mkdir(tv_directory+path_show)
+except  FileExistsError:
+    pass
+# move files
+for path in paths_discs_new:
+    shutil.move(path,tv_directory+path_show+'/'+path.name)
+
 # update kodi database
 # check database and remove files from wanted list
