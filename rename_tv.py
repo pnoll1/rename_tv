461e7178cf1f1c5eb3ab8269b14a7d2545e26b02
pat <pat@desktop.lan>
2019-02-03 01:20:53 -0800

    added 0 mode for usage with MakeMKV

--- rename_tv.py
+++ rename_tv.py
@@ -11,14 +11,17 @@ import shutil
 from pathlib import Path
 from sys import argv
 import requests
+import re
 
 directory_video = Path('/home/pat/Videos/')
-
+tv_directory = Path('/home/pat/NAS/TV/')
 
 def generate(episodes):
     '''input: comma seperarted list of episodes on each disc
     output: dictionary with keys that are disc numbers and
-    values of matching strings for episodes on each disc'''
+    values of matching strings for episodes on each disc
+    org_type = 1 is multiple epsisode per file
+    org_type = 0 is 1 episode per file in folders by disc'''
     targets = {}
     n = 1
     m = 0
@@ -58,32 +61,79 @@ def s_a_d(directory, targets):
                 paths_new.append(path_new)
     return paths_original,paths_new
 
+def s_g_d(directory, episodes):
+    '''grabs folder name, seperates out relevant info, generates new filenames,
+    replaces old filenames with new'''
+    # grab folder name
+    for filename in sorted(directory.iterdir()):
+        path_show = filename
+        break
+    # grab show name from folder name
+    # grab season from folder name
+    path_show = path_show.stem
+    season_number = path_show[-3]
+    show_list = []
+    disc_identifier = '_S'+str(season_number)+'D1'
+    show_name = path_show.replace(disc_identifier,'')
+    # construct replacement strings        
+    for i in range(1, (episodes+1)):
+        episode = 'E' + str(i)
+        path_show = Path(show_name +'_S' + season_number + episode + '.mkv')
+        show_list.append(path_show)
+    # create folder on server if needed
+    try:
+        os.mkdir(tv_directory.joinpath(show_name))
+    except FileExistsError:
+        pass
+    j = 0
+    paths =[]
+    # loop through each disc folder, low to high
+    for directory_name in sorted(directory.iterdir()):
+        os.chdir(directory_name)
+        # loop through each file in folder, low to high
+        for filename in sorted(Path.cwd().iterdir()):
+            # rename each file, then move
+            os.rename(filename, show_list[j])
+            print('Transferring file {}'.format(str(j+1)))
+            shutil.move(show_list[j],tv_directory.joinpath(show_name,show_list[j]))
+            print('Finished Transferring file {}'.format(str(j+1)))
+            paths.append(show_list[j])
+            if j == episodes:
+                break
+            j += 1
+    return paths, path_show
 # translates command line arguments into format for generate
+org_type = argv[2]
 episode_string = argv[1]
 episode_list = episode_string.split(',') # list containing strings
 # runs generate to create dictionary for what to find and replace
-episode_list_formatted = generate(episode_list)
-# remove map files
-os.chdir(directory_video)
-os.system('rm *.map')
-# renames files to Kodi format and keeps paths for processing
-paths_discs_original, paths_discs_new = s_a_d(directory_video, episode_list_formatted)
-# operations to get show name
-disc_number = list(episode_list_formatted.keys())
-path_show = paths_discs_original[0].stem
-season = path_show[-3]
-for d in disc_number:
-    disc_identifier = '_S'+str(season)+d
-    path_show = path_show.replace(disc_identifier,'')
-# create folder on server if needed
-tv_directory = '/home/pat/NAS/TV/'
-try:
-    os.mkdir(tv_directory+path_show)
-except  FileExistsError:
-    pass
-# move files
-for path in paths_discs_new:
-    shutil.move(path,tv_directory+path_show+'/'+path.name)
+if org_type == 1:
+    episode_list_formatted = generate(episode_list, org_type)
+    # remove map files
+    os.chdir(directory_video)
+    os.system('rm *.map')
+    # renames files to Kodi format and keeps paths for processing
+    paths_discs_original, paths_discs_new = s_a_d(directory_video, episode_list_formatted)
+    # operations to get show name
+    disc_number = list(episode_list_formatted.keys())
+    path_show = paths_discs_original[0].stem
+    season = path_show[-3]
+    for d in disc_number:
+        disc_identifier = '_S'+str(season)+d
+        path_show = path_show.replace(disc_identifier,'')
+    # create folder on server if needed
+    try:
+        os.mkdir(tv_directory+path_show)
+    except  FileExistsError:
+        pass
+    # move files
+    for path in paths_discs_new:
+        shutil.move(path,tv_directory+path_show+'/'+path.name)
+else:
+    episodes = 0
+    for i in episode_list:
+        episodes += int(i)
+    paths, path_show = s_g_d(directory_video, episodes)
 
 # Tell Kodi to scan for new video files
 try:
@@ -91,7 +141,7 @@ try:
 except  requests.exceptions.RequestException as e:
     print("Couldn't reach Desktop Kodi")
 try:
-    req2 = requests.get('http://192.168.1.6:8080/jsonrpc?request={"jsonrpc":"2.0","method":"VideoLibrary.Scan","id":1}')
+    req2 = requests.get('http://192.168.1.11:8080/jsonrpc?request={"jsonrpc":"2.0","method":"VideoLibrary.Scan","id":1}')
 except  requests.exceptions.RequestException as e:
     print("Couldn't reach RPi")
 # check database and remove files from wanted list
